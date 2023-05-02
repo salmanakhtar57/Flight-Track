b1c3f8c7ce1e4a8cd59b15a6ca6be42a56930e7b
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-05-02 21:17:22 +0500

    updated view file in flights

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import FLight
 
 # Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": FLight.objects.all()
+    })
