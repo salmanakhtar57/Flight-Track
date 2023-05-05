51c34d749bd0abba912aa87e0237f4af9fe0d447
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-05-05 10:44:56 +0500

    added passengers to admin page

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Flight, Airport
+from .models import Flight, Airport, Passenger
 # Register your models here.
 
 admin.site.register(Airport)
 admin.site.register(Flight) # This will add the Flight model to the admin page.
+admin.site.register(Passenger) # This will add the Passenger model to the admin page.
