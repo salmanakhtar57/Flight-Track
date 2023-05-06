1d53bef503e9193d5d2ca18330774c3ad4afcd4d
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-05-06 14:48:12 +0500

    created new app users

--- airline/users/views.py
+++ airline/users/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
 
 def login_view(request):
     if request.method == "POST":
