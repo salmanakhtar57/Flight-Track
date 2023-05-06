5b3813360c3b58a319e9902b886d0b2aee89ccbc
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-05-06 14:31:11 +0500

    created new app users

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Flight, Airport, Passenger
 # Register your models here.
 
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
 admin.site.register(Airport)
 admin.site.register(Flight) # This will add the Flight model to the admin page.
 admin.site.register(Passenger) # This will add the Passenger model to the admin page.
