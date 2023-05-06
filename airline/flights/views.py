be10540adfd417c5eb790438d6e47ece987a2d6b
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-05-06 14:19:49 +0500

    created function and update the files views.py and urls.py accordingly

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight
+from .models import Flight, Passenger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -13,3 +15,10 @@ def flight(request, flight_id):
         "flight": flight,
         "passengers": flight.passengers.all(),
     })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
